49f437a94c6288913e13bee166065deb483618f8
kaylee16357 <81766599+kaylee16357@users.noreply.github.com>
2023-11-27 10:53:06 -0800

    Create form-validation.js

---
+++ form-validation.js
@@ -0,0 +1,9 @@
+const email = document.getElementById("email");
+
+email.addEventListener("input", (event) => {
+  if (email.validity.typeMismatch) {
+    email.setCustomValidity("I am expecting an email address!");
+  } else {
+    email.setCustomValidity("");
+  }
+});
