178172c0a961bff154310af72aa26cd138d47655
kaylee16357 <81766599+kaylee16357@users.noreply.github.com>
2023-11-27 10:59:13 -0800

    Update form-validation.js

--- form-validation.js
+++ form-validation.js
@@ -1,9 +1,34 @@
 const email = document.getElementById("email");
 
+
 email.addEventListener("input", (event) => {
-  if (email.validity.typeMismatch) {
-    email.setCustomValidity("I am expecting an email address!");
+  if (email.validity.valid) {
+    // In case there is an error message visible, if the field
+    // is valid, we remove the error message.
+    emailError.textContent = ""; // Reset the content of the message
+    emailError.className = "error"; // Reset the visual state of the message
   } else {
-    email.setCustomValidity("");
+    // If there is still an error, show the correct error
+    showError();
   }
 });
+
+form.addEventListener("submit", (event) => {
+  // if the email field is valid, we let the form submit
+  if (!email.validity.valid) {
+    // If it isn't, we display an appropriate error message
+    showError();
+    // Then we prevent the form from being sent by canceling the event
+    event.preventDefault();
+  }
+});
+
+function showError() {
+  if (email.validity.valueMissing) {
+    emailError.textContent = "You need to enter an email address.";
+  } else if (email.validity.typeMismatch) {
+    emailError.textContent = "Entered value needs to be an email address.";
+  } 
+  // Set the styling appropriately
+  emailError.className = "error active";
+}
