4a6706a541e500ccfb287a7bb26a5d4ae45d544f
kaylee16357 <81766599+kaylee16357@users.noreply.github.com>
2023-11-27 09:51:38 -0800

    Update theme.js

--- theme.js
+++ theme.js
@@ -1,31 +1,36 @@
-
-function updateButtonText({ button, isDark }) {
-  const newText = isDark ? "Change to light theme" : "Change to dark theme";
-  button.innerText = newCta;
+function updateButton({ buttonEl, isDark }) {
+  const newCta = isDark ? "Change to light theme" : "Change to dark theme";
+  // use an aria-label if you are omitting text on the button
+  // and using a sun/moon icon, for example
+  buttonEl.innerText = newCta;
 }
 
-function updateThemeOnHtml({ theme }) {
+function updateThemeOnHtmlEl({ theme }) {
   document.querySelector("html").setAttribute("data-theme", theme);
 }
 
 
+/**
+* On page load:
+*/
+
+/**
+* 1. Grab what we need from the DOM and system settings on page load
+*/
 const button = document.querySelector("[data-theme-toggle]");
 
-
-updateButton({ buttonEl: button, isDark: currentThemeSetting === "dark" });
-updateThemeOnHtmlEl({ theme: currentThemeSetting });
-
-
+/**
+* 4. Add an event listener to toggle the theme
+*/
 button.addEventListener("click", (event) => {
   const newTheme = currentThemeSetting === "dark" ? "light" : "dark";
 
   localStorage.setItem("theme", newTheme);
-  updateButtonText({ buttonEl: button, isDark: newTheme === "dark" });
-  updateThemeOnHtml({ theme: newTheme });
+  updateButton({ buttonEl: button, isDark: newTheme === "dark" });
+  updateThemeOnHtmlEl({ theme: newTheme });
 
   currentThemeSetting = newTheme;
 }); 
-
 
 
 
