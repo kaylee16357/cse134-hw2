1cb36c6f039b588deeb84186b6b99c6f2cf733cc
kaylee16357 <81766599+kaylee16357@users.noreply.github.com>
2023-11-27 09:44:15 -0800

    Update theme.js

--- theme.js
+++ theme.js
@@ -1,11 +1,33 @@
-function themeSwitcher() {
 
-    const toggle = document.getElementById('modeToggle');
-
-    toggle.addEventListener('change', () => {
-      if (toggle.checked) {
-        document.documentElement.setAttribute('data-theme', 'dark');
-      }
-    }
-
+function updateButtonText({ button, isDark }) {
+  const newText = isDark ? "Change to light theme" : "Change to dark theme";
+  button.innerText = newCta;
 }
+
+function updateThemeOnHtml({ theme }) {
+  document.querySelector("html").setAttribute("data-theme", theme);
+}
+
+
+const button = document.querySelector("[data-theme-toggle]");
+
+
+updateButton({ buttonEl: button, isDark: currentThemeSetting === "dark" });
+updateThemeOnHtmlEl({ theme: currentThemeSetting });
+
+
+button.addEventListener("click", (event) => {
+  const newTheme = currentThemeSetting === "dark" ? "light" : "dark";
+
+  localStorage.setItem("theme", newTheme);
+  updateButtonText({ buttonEl: button, isDark: newTheme === "dark" });
+  updateThemeOnHtml({ theme: newTheme });
+
+  currentThemeSetting = newTheme;
+}); 
+
+
+
+
+
+
