79c8f6a9fa8e5457df0db85e530a334a9f0e7c5a
kaylee16357 <81766599+kaylee16357@users.noreply.github.com>
2023-11-27 12:19:52 -0800

    Update counter.js

--- counter.js
+++ counter.js
@@ -4,21 +4,7 @@ let char = document.getElementById('char');
 
 area.addEventListener('input', function () { 
     let content = this.value; 
-
-    if(content == 250) {
-       char.style.color = 'red';
-       char.textContent = 'Error' + '250 - content.length'; 
-    }
-    if (content >= 230 {
-       char.style.color = 'red';
-       char.textContent = 250 - content.length;   
-    }
-    else {
-      char.textContent = 250 - content.length;
-    }
-   
+    char.textContent = 250 - content.length;
     content.trim(); 
     console.log(content);  
-  
-
 });
