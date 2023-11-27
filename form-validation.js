20b644f644736b44f011aecd108f6eb06bbfcc8f
kaylee16357 <81766599+kaylee16357@users.noreply.github.com>
2023-11-27 15:24:45 -0800

    Update form-validation.js

--- form-validation.js
+++ form-validation.js
@@ -1,11 +1,9 @@
     const form  = document.getElementsByTagName('form')[0];
-    const email = document.getElementById('email');
-    const name = document.getElementById('name');
+    const email = document.getElementById('email'); 
     const emailError = document.querySelector('#email + span.error');
-    const nameError = document.querySelector('#name + span.error');
+
 
     email.addEventListener('input', function (e) {
-      
       if (email.validity.valid) {
         emailError.innerHTML = ''; 
         emailError.className = 'error'; 
@@ -14,15 +12,7 @@
       }
     });
 
- name.addEventListener('input', function (e) {
-      
-      if (name.validity.valid) {
-        nameError.innerHTML = ''; 
-        nameError.className = 'error'; 
-      } else {
-        nameError();
-      }
-    });
+ 
 
     form.addEventListener('submit', function (e) {
       if(!email.validity.valid) {
@@ -38,15 +28,6 @@
         emailError.textContent = 'Entered value is not an email';
       }
       emailError.className = 'error active';
-    }
-
- function nameError() {
-      if(name.validity.valueMissing) {
-        nameError.textContent = 'Please input your name.';
-      } else if(name.validity.tooShort) {
-        nameError.textContent = `Name should be at least ${ name.minLength } characters, you entered ${ name.value.length }.`;
-      }
-     
+    }          
       emailError.className = 'error active';
-      nameError.className = 'error active';
     }
