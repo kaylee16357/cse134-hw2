4c1681ece5613a0d9876dabef983317ac1fa8182
kaylee16357 <81766599+kaylee16357@users.noreply.github.com>
2023-11-27 14:48:18 -0800

    Update form-validation.js

--- form-validation.js
+++ form-validation.js
@@ -1,29 +1,40 @@
     const form  = document.getElementsByTagName('form')[0];
     const email = document.getElementById('email');
+    const name = document.getElementById('name');
     const emailError = document.querySelector('#email + span.error');
 
-    email.addEventListener('input', function (event) {
+    email.addEventListener('input', function (e) {
       
       if (email.validity.valid) {
         emailError.innerHTML = ''; 
         emailError.className = 'error'; 
       } else {
-        showError();
+        giveError();
       }
     });
 
-    form.addEventListener('submit', function (event) {
+ email.addEventListener('input', function (e) {
+      
+      if (email.validity.valid) {
+        emailError.innerHTML = ''; 
+        emailError.className = 'error'; 
+      } else {
+        giveError();
+      }
+    });
+
+    form.addEventListener('submit', function (e) {
       if(!email.validity.valid) {
-        showError();
-        event.preventDefault();
+        giveError();
+        e.preventDefault();
       }
     });
 
-    function showError() {
+    function giveError() {
       if(email.validity.valueMissing) {
-        emailError.textContent = 'You need to enter an e-mail address.';
+        emailError.textContent = 'Please input an e-mail address.';
       } else if(email.validity.typeMismatch) {
-        emailError.textContent = 'Entered value needs to be an e-mail address.';
+        emailError.textContent = 'Entered value is not an email';
       }
       emailError.className = 'error active';
     }
