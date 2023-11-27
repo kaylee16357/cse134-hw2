dbb04062e184f0b0a3beeafdc6822d8887fa1059
kaylee16357 <81766599+kaylee16357@users.noreply.github.com>
2023-11-27 12:14:40 -0800

    Update counter.js

--- counter.js
+++ counter.js
@@ -1,16 +1,6 @@
 let area = document.getElementById('comments'); 
 let char = document.getElementById('char'); 
 
-function changeColor(count) {
-  if(count >== 230){
-    char.style.color = 'red';
-  }
-  if(count === 250){
-    char.style.color = 'red';
-    char.textContent = 'Error: max characters reached';
-  }
-}
-
 
 area.addEventListener('input', function () { 
   
