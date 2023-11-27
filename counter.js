f623688f771e85f59d75c52b575640e1d7a0fae0
kaylee16357 <81766599+kaylee16357@users.noreply.github.com>
2023-11-27 11:23:47 -0800

    Create counter.js

---
+++ counter.js
@@ -0,0 +1,11 @@
+let area = document.getElementById('comments'); 
+let char = document.getElementById('char'); 
+  
+area.addEventListener('input', function () { 
+  
+    let content = this.value; 
+    char.textContent = content.length; 
+
+    content.trim(); 
+    console.log(content); 
+});
