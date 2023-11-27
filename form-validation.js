c0324707263f6b497e9cc745d5bd03bb40bedfb7
kaylee16357 <81766599+kaylee16357@users.noreply.github.com>
2023-11-27 12:35:05 -0800

    Update form-validation.js

--- form-validation.js
+++ form-validation.js
@@ -1,36 +1,33 @@
-const form = document.querySelector("form")[1];
-const email = document.getElementById("email");
-const emailError = document.querySelector("#email + span.error");
+    const form  = document.getElementsByTagName('form')[1];
+    const email = document.getElementById('email');
+    const emailError = document.querySelector('#email + span.error');
 
+    email.addEventListener('input', function (event) {
+     
+      if (email.validity.valid) {
+        
+        emailError.innerHTML = ''; 
+        emailError.className = 'error'; 
+      } else {
+        showError();
+      }
+    });
 
-email.addEventListener("input", (event) => {
-  if (email.validity.valid) {
-    // In case there is an error message visible, if the field
-    // is valid, we remove the error message.
-    emailError.textContent = ""; // Reset the content of the message
-    emailError.className = "error"; // Reset the visual state of the message
-  } else {
-    // If there is still an error, show the correct error
-    showError();
-  }
-});
+    form.addEventListener('submit', function (event) {
+      if(!email.validity.valid) {  
+        showError();
+        event.preventDefault();
+      }
+    });
 
-form.addEventListener("submit", (event) => {
-  // if the email field is valid, we let the form submit
-  if (!email.validity.valid) {
-    // If it isn't, we display an appropriate error message
-    showError();
-    // Then we prevent the form from being sent by canceling the event
-    event.preventDefault();
-  }
-});
+    function showError() {
+      if(email.validity.valueMissing) {
+        emailError.textContent = 'You need to enter an e-mail address.';
+      } else if(email.validity.typeMismatch) {
+        emailError.textContent = 'Entered value needs to be an e-mail address.';
+      } else if(email.validity.tooShort) {
+        emailError.textContent = `Email should be at least ${ email.minLength } characters; you entered ${ email.value.length }.`;
+      }
 
-function showError() {
-  if (email.validity.valueMissing) {
-    emailError.textContent = "You need to enter an email address.";
-  } else if (email.validity.typeMismatch) {
-    emailError.textContent = "Entered value needs to be an email address.";
-  } 
-  // Set the styling appropriately
-  emailError.className = "error active";
-}
+      emailError.className = 'error active';
+    }
