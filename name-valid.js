a15b0909c20e6b31d72a3992dbf4c4ab52636eb4
kaylee16357 <81766599+kaylee16357@users.noreply.github.com>
2023-11-27 15:25:27 -0800

    Update name-valid.js

--- name-valid.js
+++ name-valid.js
@@ -1,1 +1,22 @@
+   const name = document.getElementById('name');
+    const nameError = document.querySelector('#name + span.error');
+
+
+name.addEventListener('input', function (e) {
+      if (name.validity.valid) {
+        nameError.innerHTML = ''; 
+        nameError.className = 'error'; 
+      } else {
+        nameError();
+      }
+    });
+
+
+ function nameError() {
+      if(name.validity.valueMissing) {
+        nameError.textContent = 'Please input your name.';
+      } else if(name.validity.tooShort) {
+        nameError.textContent = `Name should be at least ${ name.minLength } characters, you entered ${ name.value.length }.`;
+      }
+
 
