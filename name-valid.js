a90034113f3cfc9bf910b39ec910d3bf7779385d
kaylee16357 <81766599+kaylee16357@users.noreply.github.com>
2023-11-27 15:28:18 -0800

    Update name-valid.js

--- name-valid.js
+++ name-valid.js
@@ -1,8 +1,9 @@
-   const name = document.getElementById('name');
-    const nameError = document.querySelector('#name + span.error');
+const form  = document.getElementsByTagName('form')[0];
+const name = document.getElementById('name'); 
+const nameError = document.querySelector('#name + span.error');
 
 
-name.addEventListener('input', function (e) {
+    name.addEventListener('input', function (e) {
       if (name.validity.valid) {
         nameError.innerHTML = ''; 
         nameError.className = 'error'; 
@@ -11,12 +12,19 @@ name.addEventListener('input', function (e) {
       }
     });
 
+    form.addEventListener('submit', function (e) {
+      if(!name.validity.valid) {
+        nameError();
+        e.preventDefault();
+      }
+    });
 
- function nameError() {
+    function nameError() {
       if(name.validity.valueMissing) {
         nameError.textContent = 'Please input your name.';
       } else if(name.validity.tooShort) {
-        nameError.textContent = `Name should be at least ${ name.minLength } characters, you entered ${ name.value.length }.`;
+        nameError.textContent = 'Email should be at least ${name.minLength} characters; you entered ${name.value.length}.`;
       }
-
-
+  
+      nameError.className = 'error active';
+    }
