b70f6924e1684a75e90167197441b67e545b7bbc
kaylee16357 <81766599+kaylee16357@users.noreply.github.com>
2023-11-27 11:57:30 -0800

    Update counter.js

--- counter.js
+++ counter.js
@@ -1,9 +1,14 @@
 let area = document.getElementById('comments'); 
 let char = document.getElementById('char'); 
+let text = document.getElementById('counter');
 
-function changeColor() {
+function changeColor(count) {
   if(count >= 230){
-    
+    text.style.color = 'red';
+  }
+  if(count >= 250){
+    text.style.color = 'red';
+    text.textContent = 'Error: over 250 characters';
   }
 
 }
@@ -13,6 +18,7 @@ area.addEventListener('input', function () {
   
     let content = this.value; 
     char.textContent = 250 - content.length; 
+    changeColor(content);
 
     content.trim(); 
     console.log(content); 
